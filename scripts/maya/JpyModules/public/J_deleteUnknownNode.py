2335e332ff19f0bec046de7ce9d148b0d9d3d774
zhangqianju <zhangqianju@lzyco.com>
2018-07-26 16:21:03 +0800

    a

--- scripts/maya/JpyModules/public/J_deleteUnknownNode.py
+++ scripts/maya/JpyModules/public/J_deleteUnknownNode.py
@@ -6,7 +6,7 @@
 ##  @version 1.0
 ##  @date  16:46 2018/1/15
 #  History:  
-
+##以前制作资产的人电脑装了一些不相干的插件,信息就回保留下来,包括他导入了别人的文件,那别人文件里的插件信息也会引入进来,最后就会有很多垃圾信息留在文件里,其实这些插件你可能都没有安装过
 import maya.cmds as cmds
 def J_deleteUnknownNode():
     cmds.delete(cmds.ls(type="unknown"))
