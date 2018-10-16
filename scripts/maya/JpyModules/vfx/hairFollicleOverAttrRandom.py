ce7db6db45cd7db15d88ec608320fab5f1fa5061
wangziheng0426 <wangziheng0426@163.com>
2018-10-16 18:07:19 +0800

    ne

---
+++ scripts/maya/JpyModules/vfx/hairFollicleOverAttrRandom.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+##  @package public
+#
+##  @brief  随机修改毛囊动力学参数属性
+##  @author 桔
+##  @version 1.0
+##  @date  16:46 2018/1/15
+#  History:  
+##随机修改毛囊动力学参数属性
+import maya.cmds as cmds
+sel=cmds.ls(sl=True)
+attrToEdit={'overrideDynamics':1}
+for i in sel:
+    childOfNode=cmds.listRelatives(i,c=True,type='follicle')
+    cmds.setAttr(i+'.overrideDynamics',1)
