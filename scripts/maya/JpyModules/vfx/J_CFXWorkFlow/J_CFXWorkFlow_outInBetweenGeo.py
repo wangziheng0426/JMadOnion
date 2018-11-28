4c37771883c9f555ace8762f9fd313e127292c16
wangziheng0426 <wangziheng0426@163.com>
2018-11-28 21:48:56 +0800

    导出inbetween

--- scripts/maya/JpyModules/vfx/J_CFXWorkFlow/J_CFXWorkFlow_outInBetweenGeo.py
+++ scripts/maya/JpyModules/vfx/J_CFXWorkFlow/J_CFXWorkFlow_outInBetweenGeo.py
@@ -7,5 +7,32 @@
 ##  @date  16:46 2018/11/2
 #  History:  
 ##导入毛发
-def J_CFXWorkFlow_outInBetweenGeo():
-    print 'J_CFXWorkFlow_outInBetweenGeo'
+import maya.cmds as cmds 
+import maya.mel as mel 
+def J_CFXWorkFlow_outInBetweenGeo(sample=1,sourceGeo=[],startTime=-1,endTime=0):
+    if len(sourceGeo)<1:
+        sourceGeo=cmds.ls(sl=True)
+    if len(sourceGeo)<1:
+        print "warring:nothing selected"
+        return;
+    gemoDupList=[]
+    for item in sourceGeo:
+        gemoDupList.append([])
+    if (startTime==-1):
+        startTime=int(cmds.playbackOptions(query=True,minTime=True))
+    if (endTime==0):
+        endTime=int(cmds.playbackOptions(query=True,maxTime=True))
+    for i in range(startTime,endTime+1,sample):
+        cmds.currentTime( i )
+        for item in range(0,len(sourceGeo),1):
+            dupGeo=cmds.duplicate(sourceGeo[item])[0]
+            childNodes=cmds.listRelatives(dupGeo,children=True,fullPath=True)
+            for item1 in childNodes:
+                if cmds.getAttr((item1+'.intermediateObject')):
+                    print item1
+                    cmds.delete(item1)
+            gemoDupList[item].append(dupGeo)
+    for item in gemoDupList:
+        cmds.blendShape(item[1:],item[0],inBetween=True)
+        cmds.delete(item[1:])
+        
