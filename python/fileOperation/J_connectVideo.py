fb2aa3b0e021cf92f11c72975843fa85bbc45c69
wangziheng0426 <wangziheng0426@163.com>
2019-03-10 09:18:31 +0800

    combin

--- python/fileOperation/J_connectVideo.py
+++ python/fileOperation/J_connectVideo.py
@@ -2,15 +2,15 @@
 import os,re
 import subprocess
 
-J_path=r'c:/cccc'
+J_path=r'c:/ccc'
 def J_connectVideo(inPath):
     inPath=inPath.decode('utf-8').replace('\\','/')
     allFile=''
-    writeFileAll=open((inPath+'/'+'run.bat'),'w')
+    writeFileAll=open((inPath+'/'+'runCombin.bat'),'w')
     for item in os.walk(inPath):
         for items in item[2]:
             if items.find('_bxxA.mp4')>-1:
-                combinFileListName=item[0].replace('\\','/')+'/'+items.replace('_bxxA.mp4','_combinJ.txt')
+                combinFileListName=item[0].replace('\\','/')+'/'+items.replace('_bxxA.mp4','_combinJ.Cbn')
                 combinFileName=item[0].replace('\\','/')+'/'+items.replace('_bxxA.mp4','_combinJ.mp4')
                 stringToFind=items.replace('A.mp4','')
                 print stringToFind
@@ -19,13 +19,13 @@ def J_connectVideo(inPath):
                 videoToCombin=''
                 for itemx in os.listdir(inPath):
                     if itemx.find(stringToFind)>-1:
-                        videoToCombin+='file '+itemx+'\n'
+                        videoToCombin+=('file '+itemx+'\n').encode('gbk')
                 
                 writeCombinFile.write(videoToCombin)
                 writeCombinFile.close()
                 
                 
-                allFile+='c:/ffmpeg.exe -f concat -i ' +combinFileListName +' -c copy '+combinFileName+'\n'
+                allFile+=('c:/ffmpeg.exe -f concat -i ' +combinFileListName +' -c copy '+combinFileName+'\n').encode('gbk')
 
 
     writeFileAll.write(allFile)
