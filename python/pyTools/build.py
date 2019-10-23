f7182a0fa6619c0f1a0d90894b7eb42d62750529
wangziheng0426 <wangziheng0426@163.com>
2019-10-23 16:51:02 +0800

    read settings from previous file

--- python/pyTools/build.py
+++ python/pyTools/build.py
@@ -1,3 +1,3 @@
 import os
-os.system('C:/Anaconda2/Scripts/pyinstaller.exe -F D:/JMadOnion/scripts/max/JpyModules/outPutTool/outPutTool.py' )
+os.system('C:/Anaconda2/Scripts/pyinstaller.exe -F D:/JMadOnion/python/pyTools/outPutTool.py' )
 os.startfile(r'C:\Program Files (x86)\Notepad++\dist')
