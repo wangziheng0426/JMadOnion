3b1dc1481554b33c58591755f725bee0d874e040
wangziheng0426 <wangziheng0426@163.com>
2019-12-04 17:33:39 +0800

    out abc

--- scripts/maya/JpyModules/vfx/J_CFXWorkFlow/J_CFXWorkFlow_outAbcGeo.py
+++ scripts/maya/JpyModules/vfx/J_CFXWorkFlow/J_CFXWorkFlow_outAbcGeo.py
@@ -13,18 +13,30 @@ import shutil
 import json
 import maya.mel as mel
 import maya.cmds as cmds
-def J_CFXWorkFlow_outAbcGeo():
+def J_CFXWorkFlow_outAbcGeo(cacheFileName='',model=0):
     filePath=cmds.file(query=True,sceneName=True).replace(cmds.file(query=True,sceneName=True,shortName=True),'')
-    cacheFileName=cmds.file(query=True,sceneName=True,shortName=True)[0:-3]
+    if cacheFileName =='':
+        cacheFileName=cmds.file(query=True,sceneName=True,shortName=True)[0:-3]
     selectNodes=cmds.ls(sl=True,long=True)
+    
+    if not os.path.exists(filePath+cacheFileName+'_cache/'):
+            os.makedirs(filePath+cacheFileName+'_cache/')
     if len(selectNodes)<1:
         cmds.confirmDialog(title=u'错误',message=u'   未选中任何节点   ',button='666')
         return
-    exportString='AbcExport -j "-frameRange '+str(cmds.playbackOptions(query=True,minTime=True))+' '+str(cmds.playbackOptions(query=True,maxTime=True))+' -uvWrite -dataFormat hdf '
-    for item in selectNodes:
-        exportString+=' -root '+item
-    if not os.path.exists(filePath+cacheFileName+'_cache/'):
-        os.makedirs(filePath+cacheFileName+'_cache/')
-    exportString+=' -file '+filePath+cacheFileName+'_cache/'+cacheFileName+'_Cloth.abc"'
-    mel.eval(exportString)
+    if model==0:
+        exportString='AbcExport -j "-frameRange '+str(cmds.playbackOptions(query=True,minTime=True))+' '+str(cmds.playbackOptions(query=True,maxTime=True))+' -uvWrite -dataFormat hdf '
+        for item in selectNodes:
+            exportString+=' -root '+item
+        
+        exportString+=' -file '+filePath+cacheFileName+'_cache/'+cacheFileName+'_Cloth.abc"'
+        mel.eval(exportString)
+        os.startfile(filePath+cacheFileName+'_cache/')
+    if model==1:
+        for item in selectNodes:
+            exportString='AbcExport -j "-frameRange '+str(cmds.playbackOptions(query=True,minTime=True))+' '+str(cmds.playbackOptions(query=True,maxTime=True))+' -uvWrite -dataFormat hdf '
+            exportString+=' -root '+item
+            exportString+=' -file '+filePath+cacheFileName+'_cache/'+item.split('|')[-1]+'.abc"'
+            mel.eval(exportString)
+            
     os.startfile(filePath+cacheFileName+'_cache/')
