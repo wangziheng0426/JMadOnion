d9abbdb6287133dfcbbc13a88e8cd117a16f49a5
wangziheng0426 <wangziheng0426@163.com>
2019-12-05 02:46:43 +0800

    add log

--- scripts/maya/JpyModules/vfx/J_CFXWorkFlow/J_CFXWorkFlow_outAbcGeo.py
+++ scripts/maya/JpyModules/vfx/J_CFXWorkFlow/J_CFXWorkFlow_outAbcGeo.py
@@ -18,26 +18,37 @@ def J_CFXWorkFlow_outAbcGeo(cacheFileName='',model=0):
     if cacheFileName =='':
         cacheFileName=cmds.file(query=True,sceneName=True,shortName=True)[0:-3]
     selectNodes=cmds.ls(sl=True,long=True)
+    finalPath=filePath+cacheFileName+'_cache/'
+    if not os.path.exists(finalPath):
+            os.makedirs(finalPath)
+    logFile=finalPath+'abcLog.txt'
+    logStr={}
+    if os.path.exists(logFile):
+        fileId=open(logFile,'r')
+        logStr=json.load(fileId)
+        fileId.close()
     
-    if not os.path.exists(filePath+cacheFileName+'_cache/'):
-            os.makedirs(filePath+cacheFileName+'_cache/')
     if len(selectNodes)<1:
         cmds.confirmDialog(title=u'错误',message=u'   未选中任何节点   ',button='666')
         return
     if model==0:
+        logStr[cacheFileName]=[]
         exportString='AbcExport -j "-frameRange '+str(cmds.playbackOptions(query=True,minTime=True))+' '+str(cmds.playbackOptions(query=True,maxTime=True))+' -uvWrite -dataFormat hdf '
         for item in selectNodes:
             exportString+=' -root '+item
-        
-        exportString+=' -file '+filePath+cacheFileName+'_cache/'+cacheFileName+'_Cloth.abc"'
+            logStr[cacheFileName].append (item)
+        exportString+=' -file '+finalPath+cacheFileName+'.abc"'
         mel.eval(exportString)
-        os.startfile(filePath+cacheFileName+'_cache/')
+        
     if model==1:
         for item in selectNodes:
             exportString='AbcExport -j "-frameRange '+str(cmds.playbackOptions(query=True,minTime=True))+' '+str(cmds.playbackOptions(query=True,maxTime=True))+' -uvWrite -dataFormat hdf '
             exportString+=' -root '+item
-            exportString+=' -file '+filePath+cacheFileName+'_cache/'+item.split('|')[-1].replace(':','@')+'.abc"'
-
+            exportString+=' -file '+finalPath+item.split('|')[-1].replace(':','@')+'.abc"'
+            logStr[item.split('|')[-1].replace(':','@')]=[]
+            logStr[item.split('|')[-1].replace(':','@')].append (item)
             mel.eval(exportString)
-            
-    os.startfile(filePath+cacheFileName+'_cache/')
+    fileId=open(logFile,'w')
+    fileId.write(json.dumps(logStr))
+    fileId.close()        
+    os.startfile(finalPath)
