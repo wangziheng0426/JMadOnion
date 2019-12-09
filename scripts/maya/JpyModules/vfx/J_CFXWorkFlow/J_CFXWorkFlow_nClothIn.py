a2061ae551cdaa66509a591d9eb260a297bfa179
wangziheng0426 <wangziheng0426@163.com>
2019-12-09 23:52:23 +0800

    hud

--- scripts/maya/JpyModules/vfx/J_CFXWorkFlow/J_CFXWorkFlow_nClothIn.py
+++ scripts/maya/JpyModules/vfx/J_CFXWorkFlow/J_CFXWorkFlow_nClothIn.py
@@ -9,8 +9,12 @@
 ##导入布料
 import maya.cmds as cmds 
 import maya.mel as mel 
+import json
 def J_CFXWorkFlow_nClothIn():
-    cacheFileName = cmds.fileDialog2(fileMode=1, caption="Import hair")
+    abcLog = cmds.fileDialog2(fileMode=1, caption="Import abcLog")
     #导入abc
+    fileId=open(abcLog,'r')
+    info=json.load(fileId)
+    
     if  cacheFileName  is not None:
         abcNode=mel.eval('AbcImport -mode import "'+cacheFileName[0] +'";')
