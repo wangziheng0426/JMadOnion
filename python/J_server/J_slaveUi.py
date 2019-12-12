5e6f6632c782cc38c18fa8015e2bc842903477d0
wangziheng0426 <wangziheng0426@163.com>
2019-12-12 18:45:15 +0800

    j_slave

--- python/J_server/J_slaveUi.py
+++ python/J_server/J_slaveUi.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'J_centerServerUi.ui'
+# Form implementation generated from reading ui file 'J_slaveUi.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
 #
