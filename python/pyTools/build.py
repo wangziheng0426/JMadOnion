dd9159c7a09069f8ea8d63e46b8c42b63d932cf0
wangziheng0426 <wangziheng0426@163.com>
2020-07-03 17:59:49 +0800

    autossh

--- python/pyTools/build.py
+++ python/pyTools/build.py
@@ -1,3 +1,3 @@
 import os
-os.system('C:/Anaconda2/Scripts/pyinstaller.exe -w -F D:/JMadOnion/J_Living/J_LivingScript/python/J_getAnimationLength.py' )
+os.system('C:/Anaconda2/Scripts/pyinstaller.exe  -F D:/JMadOnion/python/pyTools/J_getAnimationLength.py' )
 os.startfile(r'C:\Program Files (x86)\Notepad++\dist')
