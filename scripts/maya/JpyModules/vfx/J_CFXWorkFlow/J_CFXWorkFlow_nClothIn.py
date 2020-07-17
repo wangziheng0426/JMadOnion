a9604082d2f529b0100c61fdef7c6273f06b9f24
wangziheng0426 <wangziheng0426@163.com>
2020-07-17 17:01:42 +0800

    one key cache

--- scripts/maya/JpyModules/vfx/J_CFXWorkFlow/J_CFXWorkFlow_nClothIn.py
+++ scripts/maya/JpyModules/vfx/J_CFXWorkFlow/J_CFXWorkFlow_nClothIn.py
@@ -25,23 +25,36 @@ def J_CFXWorkFlow_nClothIn():
     fileId.close()
     if cmds.objExists(prFxName+'J_clothCache'):
         cmds.delete(prFxName+'J_clothCache')
+    if cmds.objExists(prFxName+'_cloth'):
+        cmds.delete(prFxName+'_cloth')
     groupNode=cmds.createNode('transform',name=(prFxName+'J_clothCache'))
     if  abcFile  is not None:
         abcNode=mel.eval('AbcImport -mode import -reparent '+groupNode+' \"'+abcFile +'\";')
-    allMeshs=cmds.listConnections(abcNode,type='mesh')
-    cmds.select(allMeshs)
+    allAbcMeshs=cmds.listConnections(abcNode,type='mesh')
+    cmds.select(allAbcMeshs)
     #检查是否有没找到mesh的缓存
     cacheHasNotFoundTarget=[]
-    for mesh in allMeshs:
+    for mesh in allAbcMeshs:
         state=True
         for item in clothInfo[prFxName]['geoInfo']:
             if cmds.objExists(item['abcGeo']):
                 if item['abcGeo'].find(mesh.split('|')[-1])>-1:
                     print item['abcGeo']
                     state=False
+                    cmds.transferAttributes(mesh,item['abcGeo'],transferPositions=1,transferNormals=0 
+                    ,transferUVs=0 ,transferColors=0 ,sampleSpace=4 ,sourceUvSpace="map1" ,targetUvSpace="map1"
+                    ,searchMethod=3,flipUVs=0,colorBorders=1 )
         if state:
             cacheHasNotFoundTarget.append(mesh)
-    mel.eval(runScript)
+    if len(cacheHasNotFoundTarget)>0:
+        mel.eval(runScript)
+        allClothMesh=cmds.listRelatives(prFxName+'_cloth',children=True,fullPath=True)
+        for mesh in allAbcMeshs:
+            for clothMesh in allClothMesh:
+                if clothMesh.split(prFxName)[-1].find(mesh.split('|')[-1])>-1:
+                    cmds.transferAttributes(mesh,clothMesh,transferPositions=1,transferNormals=0 
+                    ,transferUVs=0 ,transferColors=0 ,sampleSpace=4 ,sourceUvSpace="map1" ,targetUvSpace="map1"
+                    ,searchMethod=3,flipUVs=0,colorBorders=1 )
         
 #选择两个组进行操作，组内不可以有子物体，脚本会讲第一个组内的模型作为源物体，寻找第二个组中同名模型制作blendshape
 def J_CFXWorkFlow_blendToSelectGeo():
@@ -58,8 +71,8 @@ def J_CFXWorkFlow_blendToSelectGeo():
                 cmds.blendShape( temp,edit=True, weight=[(0,1.0)] )
                 
 def J_CFXWorkFlow_getMeshsFromAbc(abcNode,allMeshParent):
-    allMeshs=cmds.listConnections(abcNode,type='mesh')
-    for item in allMeshs:
+    allAbcMeshs=cmds.listConnections(abcNode,type='mesh')
+    for item in allAbcMeshs:
         par=cmds.listRelatives(item,fullPath=True,parent=True)
         if par[0]!=None:
             allMeshParent.append(par[0]+"|"+item)
