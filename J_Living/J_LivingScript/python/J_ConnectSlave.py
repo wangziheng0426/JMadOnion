c30e30282aa9eadfcac0d3f15ed3ff662f9d41d2
wangziheng0426 <wangziheng0426@163.com>
2020-08-25 16:35:37 +0800

    updata

--- J_Living/J_LivingScript/python/J_ConnectSlave.py
+++ J_Living/J_LivingScript/python/J_ConnectSlave.py
@@ -4,32 +4,31 @@ import sys, os, subprocess, shutil, time, re,xlrd,xlwt,urllib,functools,json,re
 import _winreg,socket
 class J_ConnectWorker:
     def test(self):
+        ip_port=("192.168.54.163", 6666)
+        #ip_port = ("192.168.53.3", 6666)
         jobInfo={}
-        jobInfo['job_Id']=5
+        jobInfo['job_Id']=3
         jobInfo['job_name']="a1"
         jobInfo['job_softWare']="ffmpeg"
         jobInfo['job_softWareVersion']="2018"
-        jobInfo['job_projectPath']="C:/test"
-        jobInfo['job_workFile']="a1.avi"
+        jobInfo['job_projectPath']="\\\\192.168.53.3/J_share/learn/pdg/201911"
+        jobInfo['job_workFile']="102目标与计划.mp4"
         jobInfo['job_scriptFile']=''
         jobInfo['job_state']="waiting"
-        job_Args=['-i "C:/test/a1.avi" -ss 0:0:0  -c:v hevc  -crf 22  -y "C:/test/a1_jc.mp4"']
+        jobInfo['job_args']=['-i "\\\\192.168.53.3\\J_share\\learn\\pdg\\201911\\102目标与计划.mp4" -ss 0:0:0  -c:v hevc  -crf 22  -y "\\\\192.168.53.3\\J_share\\learn\pdg\\201911\\102目标与计划_001.mp4"']
 
-        self.job_operation(("192.168.53.3", 6666), "add_job", 1,"test","maya.exe","path","file","script","state",[])
-        #self.job_operation(("192.168.53.3", 6666), "add_job", 2, "test","maya.exe", "path", "file", "script", "state", [])
-        #self.job_operation(("192.168.53.3", 6666), "add_job", 4, "test","maya.exe", "path", "file", "script", "state", [])
         #self.job_operation(("192.168.53.3", 6666), "add_job",jobInfo,job_Args)
-        #self.job_operation(("192.168.53.3", 6666), "add_job", 6, "a2", "ffmpeg","2018","C:/test", "a2.avi", "", "waiting", ['-i "C:/test/a2.avi" -ss 0:0:0  -c:v hevc  -crf 22  -y "C:/test/a2_jc.mp4"'])
-        #self.job_operation(("192.168.53.3", 6666), "add_job", 7, "a3", "ffmpeg","2018", "C:/test", "a3.mp4", "", "waiting", ['-i "C:/test/a3.mp4" -ss 0:0:0  -c:v hevc  -crf 22  -y "C:/test/a3_jc.mp4"'])
-        #self.job_operation(("192.168.53.3", 6666), "add_job", 8, "a4", "ffmpeg", "2018", "C:/test", "a4.mp4","", "waiting", ['-i "C:/test/a4.mp4" -ss 0:0:0  -c:v hevc  -crf 22  -y "C:/test/a4_jc.mp4"'])
-        #self.job_operation(("192.168.53.3", 6666),'start_worker',jobInfo,[])
+
+
+        self.job_operation(ip_port, "add_job",jobInfo)
+
+        self.job_operation(ip_port,'start_worker',jobInfo)
         #time.sleep(5)
-        self.get_JobList()
+        self.get_JobList(ip_port)
 
-    def get_JobList(self):
+    def get_JobList(self,ip_port):
         client = socket.socket()
-        client.connect(("192.168.53.3", 6666))
-
+        client.connect(ip_port)
         client.send("get_job_list")
         temp = (client.recv(4096)).decode('utf-8').encode('gbk')
         while (temp != "" and temp!="list_ended"):
@@ -38,8 +37,8 @@ class J_ConnectWorker:
             temp = (client.recv(4096)).decode('utf-8').encode('gbk')
         client.shutdown(socket.SHUT_RDWR)
         client.close()
-    def job_operation(self,ip_port,job_type,job_info,job_args):
-        keys=['job_Id','job_name','job_softWare','job_softWareVersion','job_projectPath','job_workFile','job_scriptFile','job_state']
+    def job_operation(self,ip_port,job_type,job_info):
+        keys=['job_Id','job_name','job_softWare','job_softWareVersion','job_projectPath','job_workFile','job_scriptFile','job_state','job_args']
         #检查传入数据
         for key in keys:
             if not job_info.has_key(key):
