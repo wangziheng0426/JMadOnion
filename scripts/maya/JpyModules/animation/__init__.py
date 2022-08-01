11cc5093a8793c19b45129ad3248576f51de6a28
wangziheng0426 <wangziheng0426@163.com>
2022-08-01 18:50:26 +0800

    blendshape tool

--- scripts/maya/JpyModules/animation/__init__.py
+++ scripts/maya/JpyModules/animation/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 import J_viewKeyTool
 import J_playBlast
-
+import J_animationExporter
 from J_exportSelections2json import *
 
--- scripts/maya/JpyModules/model/__init__.py
+++ scripts/maya/JpyModules/model/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding:utf-8 -*-
 from J_meshRandomColor import *
+import J_blendTools  
 import J_uvTools  
