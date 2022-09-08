a12dc04998f86cb9503e36cc8b16b2a96cdc2548
wangziheng0426 <wangziheng0426@163.com>
2022-09-08 17:20:03 +0800

    new tool

--- scripts/maya/JpyModules/model/J_blendTools/J_combinBlendShape.py
+++ scripts/maya/JpyModules/model/J_blendTools/J_combinBlendShape.py
@@ -11,6 +11,7 @@
 import maya.cmds as cmds
 import maya.api.OpenMaya as om2
 import maya.mel as mel
+import csv
 
 def J_combinBlendShape(mode=0):
     allModel=cmds.ls(sl=True)
@@ -36,6 +37,27 @@ def J_combinBlendShape(mode=0):
                     if blendAttr1.find(blAttrName)>-1:
                         cmds.setAttr(blNode+'.'+blendAttr1,0)      
 
+def J_combinBlendShapeCsv():
+    csvfile=cmds.fileDialog2(fileMode=1, caption="cvs")[0]
+    fileTemp=open(csvfile,'r')
+    dicCsv=csv.DictReader(fileTemp )
 
+    allModel=cmds.ls(sl=True)
+    cModel=cmds.polyUnite(n=cmds.ls(sl=True)[0]+"_combin")
+    cMesh=cmds.ls(cModel,type='transform')
+    cBlendShape=cmds.ls(cmds.listHistory(cMesh),type="blendShape")
+    if len(cBlendShape)<1:
+        return
+    blendShapeWeightList = cmds.listAttr(cBlendShape[0]+'.w', m=True)
+    for facial in dicCsv:
+        for k,v in facial.items():
+            for blNode in cBlendShape:
+                blendShapeWeightList1 = cmds.listAttr(blNode+'.w', m=True)
+                for blendAttr1 in blendShapeWeightList1:
+                    if blendAttr1.find(k)>-1:
+                        cmds.setAttr(blNode+'.'+blendAttr1,v)
+        cmds.duplicate(cMesh[0],n=(cMesh[0][:4]+"_"+k))
+
+    fileTemp.close()
 if __name__=='__main__':
     J_combinBlendShape()
