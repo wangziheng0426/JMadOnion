6f30d367c303db1b6d9ba593042a8bf272b75443
wangziheng0426 <wangziheng0426@163.com>
2022-10-20 15:37:45 +0800

    ak

--- scripts/maya/JpyModules/animation/J_visualCharacterTool/J_loadJsonKeyFrame.py
+++ scripts/maya/JpyModules/animation/J_visualCharacterTool/J_loadJsonKeyFrame.py
@@ -36,10 +36,35 @@ def J_loadJsonKeyFrame():
                         cmds.setKeyframe(ctrlName[0],t=float(v1)*frameRate,v=float(item['values'][str(vId+1)]),attribute='translateY')
                         cmds.setKeyframe(ctrlName[0],t=float(v1)*frameRate,v=float(item['values'][str(vId+2)]),attribute='translateZ')
     else:
-        for obj in sel:
-            blendShapes=cmds.ls(cmds.listHistory(cmds.ls(sl=True)),type='blendShape')
-            for bs in blendShapes:
-                for k,v in ss.items():
-                    if cmds.attributeQuery(k,node=bs,ex=True):
-                        for index in range(0,len(v)-1):
-                            cmds.setKeyframe(bs+'.'+k,t=index,v=v[index])
+         for k,v in ss.items():
+            for obj in cmds.ls(cmds.listHistory(sel),type='blendShape'):
+                for item in v:
+                    blendShapeName=item['name']
+                    if cmds.attributeQuery(blendShapeName,node=obj,ex=True):                        
+                        for k1,v1 in item['times'].items():
+                            cmds.setKeyframe(obj+'.'+blendShapeName,t=float(v1)*frameRate,v=float(item['values'][k1]))
+                        
+def J_loadAIJsonKeyFrame():
+    j_jsonFile = cmds.fileDialog2(fileMode=1, caption="Import keys from json")
+    if j_jsonFile is None:
+        return
+    fileName=j_jsonFile[0]
+    fileI=open(fileName,'r')
+    ss=json.load(fileI)
+    fileI.close()
+    mydic={'game':15,'film':24,'pal':25,'ntsc':30,'show':48,'palf':50,'ntscf':60}
+    frameRate=cmds.currentUnit(query=True,time=True)
+    if frameRate in mydic:
+        frameRate= mydic[frameRate]
+    else:
+        frameRate=24
+    sel=cmds.ls(sl=True)
+    if len(sel)>0:
+        for bs in cmds.ls(cmds.listHistory(sel),type='blendShape'):
+            for k,v in ss.items():
+                if cmds.attributeQuery(k,node=bs,ex=True):
+                    for index in range(0,len(v)-1):
+                        cmds.setKeyframe(bs+'.'+k,t=index,v=v[index])
+
+if __name__=='__main__':
+    J_loadJsonKeyFrame()
