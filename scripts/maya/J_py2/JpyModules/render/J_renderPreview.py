2081030730531364028f8f4d05060a8b8e0761e0
zhangqianju13366449662 <zhangqianjie@CN210103665>
2023-06-06 21:39:00 +0800

    render

--- scripts/maya/J_py2/JpyModules/render/J_renderPreview.py
+++ scripts/maya/J_py2/JpyModules/render/J_renderPreview.py
@@ -10,11 +10,33 @@
 import maya.cmds as cmds
 import maya.mel as mel
 import os
-def J_renderPreview(cachePath=""):
+def J_renderPreview(lightFile="",resolution=[],camera='',animationRange=[]):
     #当前相机
     cam=cmds.modelPanel("modelPanel4",query=True,camera=True)
     #清理场景
     #关闭所有灯光
-    lights=cmds.ls(type='light')
+    lightTypes=['aiAreaLight','aiSkyDomeLight','light']
+    for item in lightTypes:
+        for lightItem in cmds.ls(type=item):
+            cmds.setAttr(lightItem+".visibility",0)
+    #导入灯光文件
+    if os.path.exists(cmds.workspace(query=True,rd=True)+'lightPresets.ma'):
+        waterMark=(cmds.workspace(query=True,rd=True)+'lightPresets.ma') 
+    if os.path.exists(lightFile):
+        cmds.file(lightFile,i=1,type="mayaAscii",ignoreVersion=1,ra=1,mergeNamespacesOnClash=1,ns=":")
+    #改分辨率
+    if (len(resolution)==2):
+        cmds.setAttr("defaultResolution.width",resolution[0])
+        cmds.setAttr("defaultResolution.height",resolution[1])
+    #开启动画
+    cmds.setAttr("defaultRenderGlobals.animation",1)
+    cmds.setAttr("defaultRenderGlobals.animationRange",1)
+
+    if (len(animationRange)==2):
+        cmds.setAttr("defaultRenderGlobals.animation",1)
+        cmds.setAttr("defaultResolution.height",resolution[1])
+    
+def renderImages():
+    pass
 if __name__=='__main__':
     J_renderPreview()                   
