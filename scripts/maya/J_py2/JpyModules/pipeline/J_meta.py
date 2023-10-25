b6aa9ac5a057b2d4f77eb90649e14153b142c91f
zhangqianju13366449662 <zhangqianjie@CN210103665>
2023-10-25 23:24:35 +0800

    jmeta operation

--- scripts/maya/J_py2/JpyModules/pipeline/J_meta.py
+++ scripts/maya/J_py2/JpyModules/pipeline/J_meta.py
@@ -7,29 +7,39 @@
 ##  @date   12:03 2023/10/25
 #  History:  
 
-import maya.cmds as cmds
-import maya.mel as mel
-import maya.api.OpenMaya as om2
 import re,os,json,uuid
 
 class J_meta():
     metaInfo={}
+    metaPath=''
     def __init__(self,inputPath):
-        print (inputPath)
-        fileo=open(inputPath,'r')
-        metaInfo=json.load(fileo)
-        fileo.close()
-        pass
-
+        #如果输入的是jmeta则直接读取，不是则新建
+        if os.path.exists(inputPath) :
+            if inputPath.endswith('.jmeta'):
+                self.metaPath=inputPath
+                self.J_loadMeta(inputPath)                
+            else:                
+                self.metaPath=os.path.splitext(inputPath)[0]+'.jmeta'
+                self.J_createMeta(self,inputPath)
+        else:
+            print (inputPath+u':不存在')
     #创建jmeta文件
-    def J_createMeta():
-        pass
+    def J_createMeta(self):        
+        self.metaInfo['baseInfo']={'uuid':'','assetType':'',\
+            'fileType':'','user':'','fullPath':'','relativePath':''}
+        self.metaInfo['userInfo']={}
+        self.J_saveMeta()
+        
     #重置
-    def J_resetMeta():
-        pass
+    def J_saveMeta(self):
+        fileo=open(self.metaPath,'r')
+        fileo.write(json.dumps(self.metaInfo,encoding='utf-8',ensure_ascii=False,sort_keys=True,indent=4,separators=(",",":")))
+        fileo.close()
     #读取
-    def J_loadMeta():
-        pass
+    def J_loadMeta(self,inputPath):
+        fileo=open(inputPath,'r')
+        self.metaInfo=json.load(fileo)
+        fileo.close()
     
 
     
--- scripts/maya/J_py2/JpyModules/pipeline/__init__.py
+++ scripts/maya/J_py2/JpyModules/pipeline/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding:utf-8 -*-
 import J_resourceExporter
 import J_projectManeger
+import J_meta
