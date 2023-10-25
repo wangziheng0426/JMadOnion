e179c6427ef7514bc49c43494675637e03ca70b8
wangziheng0426 <wangziheng0426@163.com>
2023-10-26 00:51:39 +0800

    new project manager

--- scripts/maya/J_py2/JpyModules/pipeline/J_meta.py
+++ scripts/maya/J_py2/JpyModules/pipeline/J_meta.py
@@ -7,27 +7,35 @@
 ##  @date   12:03 2023/10/25
 #  History:  
 
-import re,os,json,uuid
-
+import os,json,uuid
+#输入一个路径,如果是jmeta文件则直接读取,如果不是,则查询是否有jmeta文件,有则读取,没有则新建
 class J_meta():
     metaInfo={}
     metaPath=''
-    def __init__(self,inputPath):
-        #如果输入的是jmeta则直接读取，不是则新建
+    def __init__(self,inputPath,projectPath):
+        #如果输入的是jmeta则直接读取，不是则查找这个文件是否有meta,没有则新建
         if os.path.exists(inputPath) :
             if inputPath.endswith('.jmeta'):
                 self.metaPath=inputPath
                 self.J_loadMeta(inputPath)                
             else:                
-                self.metaPath=os.path.splitext(inputPath)[0]+'.jmeta'
-                self.J_createMeta(self,inputPath)
+                self.metaPath=inputPath+'.jmeta'
+                if os.path.exists(self.metaPath):
+                    self.J_loadMeta(self.metaPath)   
+                else:
+                    self.J_createMeta(self,inputPath,projectPath)
         else:
             print (inputPath+u':不存在')
     #创建jmeta文件
-    def J_createMeta(self):        
+    def J_createMeta(self,inputPath,projectPath):        
         self.metaInfo['baseInfo']={'uuid':'','assetType':'',\
-            'fileType':'','user':'','fullPath':'','relativePath':''}
+            'fileType':'','user':'','fullPath':'',\
+            'relativePath':'','projectPath':''}
         self.metaInfo['userInfo']={}
+        #新建meta时创建uuid,记录文件绝对目录,工程目录,相对目录
+        self.metaInfo['baseInfo']['uuid']=uuid.uuid1()
+        self.metaInfo['baseInfo']['fullPath']=inputPath
+        
         self.J_saveMeta()
         
     #重置
--- scripts/maya/J_py2/JpyModules/pipeline/J_projectManeger/J_projectManeger.py
+++ scripts/maya/J_py2/JpyModules/pipeline/J_projectManeger/J_projectManeger.py
@@ -13,108 +13,110 @@ import maya.api.OpenMaya as om2
 import re,os,json,uuid
 import JpyModules
 #
-
-def J_projectManeger_init():
-    treeV='J_projectManager_TreeView'
-    cmds.treeView( treeV, edit=True, removeAll = True )
-    projectPath=cmds.textField('J_projectManager_projectPath',q=1,text=1)
-    if projectPath.endswith('/'):
-        projectPath=projectPath[0:-1]
-        cmds.textField('J_projectManager_projectPath',e=1,text=projectPath)
-    #构建项目目录
-    cmds.treeView(treeV,edit=1, addItem=(projectPath, "") )
-    cmds.treeView(treeV,edit=1, image=(projectPath, 1,'SP_DirClosedIcon.png') )
-    cmds.treeView(treeV,edit=1, image=(projectPath, 2,'info.png') )
+class J_projectManeger():
     
-    if os.path.exists(projectPath):
-        #如果当前打开的文件在工程目录下,则创建目录结构,如果不在,就根据工程目录生产
-        sceneFileName=cmds.file(query=True,sceneName=True)
-        
-        for fitem in os.listdir(projectPath):              
-            J_projectManeger_treeAddItem(treeV,projectPath,projectPath+'/'+fitem)
-        #确认文件再工程目录下
-        if sceneFileName.startswith(projectPath):
-            projectPathTemp=projectPath
-            for pItem in os.path.dirname(sceneFileName).replace(projectPath,'').split('/'):
-                if pItem!='':
-                    projectPathTemp=projectPathTemp+'/'+pItem   
-                    J_projectManeger_doubleClick(projectPathTemp,'')
-            cmds.treeView(treeV,e=1, selectItem=(sceneFileName,True))
-            cmds.treeView(treeV,e=1, showItem=sceneFileName)
-    #设置界面命令
-    #双击命令
-    cmds.treeView(treeV,edit=1, itemDblClickCommand2=J_projectManeger_doubleClick )
-    
-    cmds.treeView(treeV,edit=1, contextMenuCommand=J_projectManeger_popupMenuCommand )
-    cmds.treeView(treeV,edit=1, pressCommand=[(2,J_projectManeger_openSubWin) ])
 
-#添加条目
-def J_projectManeger_treeAddItem(treeV,parentItem,item):
-    if not item.endswith('.json')  and not item.endswith('.jmeta') :      
-        if not cmds.treeView(treeV,q=1, itemExists=item ):
-            cmds.treeView(treeV,edit=1, addItem=(item, parentItem))
-        itemDisplayName=os.path.basename(item)
-        cmds.treeView(treeV,edit=1, displayLabel=(item, itemDisplayName))
-        #改图标
-        iconDic={'folder':'SP_DirClosedIcon.png','openfolder':'SP_DirOpenIcon.png','.ma':'kAlertQuestionIcon.png',\
-            '.mb':'kAlertQuestionIcon.png','needSave':'kAlertStopIcon.png','tex':'out_file.png','file':'SP_FileIcon',\
-            '.mov':'playblast.png','.mp4':'playblast.png' ,'.avi':'playblast.png' ,'.m4v':'playblast.png',\
-            '.fbx':'fbxReview.png','.abc':'animateSnapshot.png'}
-        splitName=os.path.splitext(item)
-        iconKey='file'
-        #分配图标
-        if splitName[1]=='':iconKey='folder'
-        if splitName[1].lower() in {".jpg",'.tga','.jpeg','tif','.png','.hdr','.tiff',}:iconKey='tex'
-        if iconDic.has_key(splitName[1]):iconKey=splitName[1]
-
-        cmds.treeView(treeV,edit=1, image=(item, 1,iconDic[iconKey]) )
-        cmds.treeView(treeV,edit=1, image=(item, 2,'polyGear.png') )
-
-#双击打开文件
-def J_projectManeger_doubleClick(itemName,itemLabel):
-    print (itemName)
-    if os.path.splitext(itemName)[1].lower()  in {".ma",'.mb','.fbx'}:
-        cmds.file(itemName,prompt=False,open=True,loadReferenceDepth='none',force=True)
-    if os.path.isdir(itemName):
+    def J_projectManeger_init():
         treeV='J_projectManager_TreeView'
-        #读取下层目录,如果已经有子集,则先清除
-        if len(cmds.treeView(treeV,q=1, children=itemName ))>1:
-            for ritem in cmds.treeView(treeV,q=1, children=itemName )[1:]:
-                if cmds.treeView(treeV,q=1, itemExists=ritem ):
-                    cmds.treeView(treeV,e=1, removeItem=ritem )
-        for fitem in os.listdir(itemName):
-            J_projectManeger_treeAddItem(treeV,itemName,itemName+'/'+fitem)
-    if os.path.splitext(itemName)[1].lower()  in {".mp4",'.avi','.mov','.m4v'}:
-        os.startfile(itemName)
-#打开文件所在目录
-def J_projectManeger_openFilePath():
-    sel=cmds.treeView('J_projectManager_TreeView',q=1, selectItem=1)
-    if len(sel)>0:
-        if os.path.isdir(sel[0]):
-            os.startfile(sel[0])
+        cmds.treeView( treeV, edit=True, removeAll = True )
+        projectPath=cmds.textField('J_projectManager_projectPath',q=1,text=1)
+        if projectPath.endswith('/'):
+            projectPath=projectPath[0:-1]
+            cmds.textField('J_projectManager_projectPath',e=1,text=projectPath)
+        #构建项目目录
+        cmds.treeView(treeV,edit=1, addItem=(projectPath, "") )
+        cmds.treeView(treeV,edit=1, image=(projectPath, 1,'SP_DirClosedIcon.png') )
+        cmds.treeView(treeV,edit=1, image=(projectPath, 2,'info.png') )
+        
+        if os.path.exists(projectPath):
+            #如果当前打开的文件在工程目录下,则创建目录结构,如果不在,就根据工程目录生产
+            sceneFileName=cmds.file(query=True,sceneName=True)
+            
+            for fitem in os.listdir(projectPath):              
+                J_projectManeger_treeAddItem(treeV,projectPath,projectPath+'/'+fitem)
+            #确认文件再工程目录下
+            if sceneFileName.startswith(projectPath):
+                projectPathTemp=projectPath
+                for pItem in os.path.dirname(sceneFileName).replace(projectPath,'').split('/'):
+                    if pItem!='':
+                        projectPathTemp=projectPathTemp+'/'+pItem   
+                        J_projectManeger_doubleClick(projectPathTemp,'')
+                cmds.treeView(treeV,e=1, selectItem=(sceneFileName,True))
+                cmds.treeView(treeV,e=1, showItem=sceneFileName)
+        #设置界面命令
+        #双击命令
+        cmds.treeView(treeV,edit=1, itemDblClickCommand2=J_projectManeger_doubleClick )
+        
+        cmds.treeView(treeV,edit=1, contextMenuCommand=J_projectManeger_popupMenuCommand )
+        cmds.treeView(treeV,edit=1, pressCommand=[(2,J_projectManeger_openSubWin) ])
+
+    #添加条目
+    def J_projectManeger_treeAddItem(treeV,parentItem,item):
+        if not item.endswith('.json')  and not item.endswith('.jmeta') :      
+            if not cmds.treeView(treeV,q=1, itemExists=item ):
+                cmds.treeView(treeV,edit=1, addItem=(item, parentItem))
+            itemDisplayName=os.path.basename(item)
+            cmds.treeView(treeV,edit=1, displayLabel=(item, itemDisplayName))
+            #改图标
+            iconDic={'folder':'SP_DirClosedIcon.png','openfolder':'SP_DirOpenIcon.png','.ma':'kAlertQuestionIcon.png',\
+                '.mb':'kAlertQuestionIcon.png','needSave':'kAlertStopIcon.png','tex':'out_file.png','file':'SP_FileIcon',\
+                '.mov':'playblast.png','.mp4':'playblast.png' ,'.avi':'playblast.png' ,'.m4v':'playblast.png',\
+                '.fbx':'fbxReview.png','.abc':'animateSnapshot.png'}
+            splitName=os.path.splitext(item)
+            iconKey='file'
+            #分配图标
+            if splitName[1]=='':iconKey='folder'
+            if splitName[1].lower() in {".jpg",'.tga','.jpeg','tif','.png','.hdr','.tiff',}:iconKey='tex'
+            if iconDic.has_key(splitName[1]):iconKey=splitName[1]
+
+            cmds.treeView(treeV,edit=1, image=(item, 1,iconDic[iconKey]) )
+            cmds.treeView(treeV,edit=1, image=(item, 2,'polyGear.png') )
+
+    #双击打开文件
+    def J_projectManeger_doubleClick(itemName,itemLabel):
+        print (itemName)
+        if os.path.splitext(itemName)[1].lower()  in {".ma",'.mb','.fbx'}:
+            cmds.file(itemName,prompt=False,open=True,loadReferenceDepth='none',force=True)
+        if os.path.isdir(itemName):
+            treeV='J_projectManager_TreeView'
+            #读取下层目录,如果已经有子集,则先清除
+            if len(cmds.treeView(treeV,q=1, children=itemName ))>1:
+                for ritem in cmds.treeView(treeV,q=1, children=itemName )[1:]:
+                    if cmds.treeView(treeV,q=1, itemExists=ritem ):
+                        cmds.treeView(treeV,e=1, removeItem=ritem )
+            for fitem in os.listdir(itemName):
+                J_projectManeger_treeAddItem(treeV,itemName,itemName+'/'+fitem)
+        if os.path.splitext(itemName)[1].lower()  in {".mp4",'.avi','.mov','.m4v'}:
+            os.startfile(itemName)
+    #打开文件所在目录
+    def J_projectManeger_openFilePath():
+        sel=cmds.treeView('J_projectManager_TreeView',q=1, selectItem=1)
+        if len(sel)>0:
+            if os.path.isdir(sel[0]):
+                os.startfile(sel[0])
+            else:
+                #os.startfile(os.path.dirname(sel[0]))
+                temp=sel[0].replace('/','\\')
+                os.system('explorer /select, '+temp)
+        print (sel)
+    #设置工程目录
+    def J_projectManeger_setProject():
+        newProjectFolder= cmds.fileDialog2(fileMode=2)
+        if newProjectFolder!=None: 
+            newProjectFolder=newProjectFolder[0]
         else:
-            #os.startfile(os.path.dirname(sel[0]))
-            temp=sel[0].replace('/','\\')
-            os.system('explorer /select, '+temp)
-    print (sel)
-#设置工程目录
-def J_projectManeger_setProject():
-    newProjectFolder= cmds.fileDialog2(fileMode=2)
-    if newProjectFolder!=None: 
-        newProjectFolder=newProjectFolder[0]
-    else:
-        return
-    cmds.textField('J_projectManager_projectPath',e=1,text=newProjectFolder)
-    mel.eval('setProject \"'+newProjectFolder+"\"")
-    J_projectManeger_init()
-#右键预制菜单
-def J_projectManeger_popupMenuCommand(itemName):
-    cmds.treeView('J_projectManager_TreeView',e=1, clearSelection=1)
-    cmds.treeView('J_projectManager_TreeView',e=1, selectItem=(itemName,True))
-    return True
-def J_projectManeger_openSubWin(itemName,itemLabel):
-    mel.eval('J_projectManeger_subWin()')
-    J_projectManeger_subWin_init(itemName)
+            return
+        cmds.textField('J_projectManager_projectPath',e=1,text=newProjectFolder)
+        mel.eval('setProject \"'+newProjectFolder+"\"")
+        J_projectManeger_init()
+    #右键预制菜单
+    def J_projectManeger_popupMenuCommand(itemName):
+        cmds.treeView('J_projectManager_TreeView',e=1, clearSelection=1)
+        cmds.treeView('J_projectManager_TreeView',e=1, selectItem=(itemName,True))
+        return True
+    def J_projectManeger_openSubWin(itemName,itemLabel):
+        mel.eval('J_projectManeger_subWin()')
+        J_projectManeger_subWin_init(itemName)
 #############################################################################################
 #子窗口逻辑
 def J_projectManeger_subWin_init(inPath):
